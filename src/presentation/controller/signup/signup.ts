f5f6852e5a8ca8700ef76bf1466aea616422b863
Henrique P. Rosa <henrique.prosa@hotmail.com>
2022-03-14 08:38:29 +0000

    feat: Added types HttpRequest and HttpResponse

--- src/presentation/controller/signup/signup.ts
+++ src/presentation/controller/signup/signup.ts
@@ -1,9 +1,10 @@
+import { HttpRequest, HttpResponse } from "../../protocols/http";
 
 export class SignUpController {
- handle (httpRequest: any) :any {
-   return {
-     statusCode: 400,
-     body: new Error('Missing param: name')
-   }
+ handle(httpRequest: HttpRequest): HttpResponse {
+  return {
+   statusCode: 400,
+   body: new Error('Missing param: name')
+  }
  }
 }
